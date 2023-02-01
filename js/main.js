fa8d07bb1dba6abe55cd15fbd44a87b3c8227251
Almudena Lloret <almudenalloret@gmail.com>
2023-02-01 11:28:07 +0100

    Add class 1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict'; 
+
+//3. Mostrar formulario 
+
+//Definimos la constante búscándola dentro del html con la clase js-new-form 
+const formHidden=document.querySelector ('.js-new-form');
+
+// Esconde la clase collapse de la constante formHidden
+formHidden.classList.remove("collapsed"); 
+
+//4. Mostrar gatitos desde JS
+
+const catList=document.querySelector(".js-list")
+git
+//dudasssss---------innerHtml???? 
