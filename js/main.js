c968ebe3c934c7f0a6445771d3382df5f4a35c37
lauramorenochico <laura.moreno.chico@gmail.com>
2023-02-03 11:24:50 +0100

    concatenate kitten

--- js/main.js
+++ js/main.js
@@ -11,86 +11,76 @@
 //4. Mostrar gatitos desde JS
 
 //const catList=document.querySelector(".js-list");
-const Catlist= document.querySelector('.js-list'); 
-
-/*const kittenOne = `<li class="card">
-<article>
-  <img
-    class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
-    alt="gatito"
-  />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
-   </p>
-</article>
-</li>`;*/
-/*const kittenTwo = `<li class="card">
-            <img
-              class="card_img"
-              src="https://dev.adalab.es/sphynx-gato.webp"
-              alt="sphynx-cat"
-            />
-            <h3 class="card_title">Fiona</h3>
-            <h4 class="card_race">Sphynx</h4>
-            <p class="card_description">
-              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-              hasta con pinta de alienígena han llegado a definir a esta raza
-              gatuna que se caracteriza por la «ausencia» de pelo.
-            </p>
-          </li>`;*/
-/*  const kittenThree = `<li class="card">
-            <img
-              class="card_img"
-              src="https://dev.adalab.es/maine-coon-cat.webp"
-              alt="maine-coon-cat"
-            />
-            <h3 class="card_title">Cielo</h3>
-            <h4 class="card_race">Maine Coon</h4>
-            <p class="card_description">
-              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-              bella mirada se ha convertido en una de sus señas de identidad.
-              Sus ojos son grandes y las orejas resultan largas y en punta.
-            </p>
-          </li>`;*/
-        
-
-//vamos a traer a Anastasio y amigos de vuelta
-//primero de todo conectar js con HTML
-//const Catlist= document.querySelector('.js-list'); //esta constante va en js-list, siempre se pone antes (lo  ideal es que siempre fuese al principio del documento)
-Catlist.innerHTML = kittenOne + kittenTwo + kittenThree; // esta constante está compuesta por
-
-//ejercicio 2.2
-const kittenOneImage = 'class="card_img'
-    src="https://dev.adalab.es/gato-siames.webp"
-    alt="gatito";
+const catList= document.querySelector('.js-list'); 
+const kittenOneImage = "https://dev.adalab.es/gato-siames.webp";
+   
 const kittenOneName = 'Anastacio';
 const kittenOneDesc =
   ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
 const kittenOneRace = 'Siamés';
-
------
-const kittenTwoImage = <img
-              class="card_img"
-              src="https://dev.adalab.es/sphynx-gato.webp"
-              alt="sphynx-cat"/>;
+const kittenTwoImage = 
+             "https://dev.adalab.es/sphynx-gato.webp";
+            
 const kittenTwoName = 'Fiona';
 const kittenTwoDesc =
   'Produce fascinación y curiosidad. Exótico, raro, bello, extraño…     hasta con pinta de alienígena han llegado a definir a esta raza        gatuna que se caracteriza por la «ausencia» de pelo';
 const kittenTwoRace = 'Sphynx';
--------
-const kittenThreeImage = <img
-              class="card_img"
-              src="https://dev.adalab.es/maine-coon-cat.webp"
-              alt="maine-coon-cat"/>;
+
+const kittenThreeImage =         
+              "https://dev.adalab.es/maine-coon-cat.webp";             ;
 const kittenThreeName = 'Cielo';
 const kittenThreeDesc =
   ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su     bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
 const kittenThreeRace = 'Maine Coon';
 
-----
- const KittenOne.innerHTML= kittenOneImage + kittenOneName + kittenOneDesc + kittenOneRace; 
+
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img" src= ${kittenOneImage}
+    alt="gatito"/>
+  <h3 class="card_title"> ${kittenOneName}</h3>
+  <h4 class="card_race"> ${kittenOneRace}</h4>
+  <p class="card_description"> ${kittenOneDesc} </p>         
+  </article>
+</li>`;
+
+const kittenTwo = `<li class="card">
+            <img
+              class="card_img"
+              src= ${kittenTwoImage}
+              alt="sphynx-cat"
+            />
+            <h3 class="card_title">${kittenTwoName}</h3>
+            <h4 class="card_race">${kittenTwoRace}</h4>
+            <p class="card_description"> ${kittenTwoDesc}              
+            </p>
+          </li>`;
+ const kittenThree = `<li class="card">
+            <img
+              class="card_img"
+              src= ${kittenThreeImage}
+              alt="maine-coon-cat"
+            />
+            <h3 class="card_title">${kittenThreeName}</h3>
+            <h4 class="card_race">${kittenThreeRace}</h4>
+            <p class="card_description">${kittenThreeDesc}
+            
+            </p>
+          </li>`;
+        
+
+//vamos a traer a Anastasio y amigos de vuelta
+//primero de todo conectar js con HTML
+//const Catlist= document.querySelector('.js-list'); //esta constante va en js-list, siempre se pone antes (lo  ideal es que siempre fuese al principio del documento)
+catList.innerHTML = kittenOne + kittenTwo + kittenThree; // esta constante está compuesta por
+
+//ejercicio 2.2
+/*const kittenOneImage = "https://dev.adalab.es/gato-siames.webp";
+   
+const kittenOneName = 'Anastacio';
+const kittenOneDesc =
+  ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+const kittenOneRace = 'Siamés';*/
+//al declarar las const las subimos arroba para que se pudan relacionar con el contenido.
+
